f29903a241596f3c5548e6f440a476744a437f09
Zihui Zhang <zihui@Xuhuis-Mac-mini.local>
2025-11-05 18:38:00 -0800

    added some new features

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,9 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
+    static ArrayList<Todo> todoList = new ArrayList<>();
+
     public static void main(String[] args) {
-        ArrayList<Todo> todoList = new ArrayList<>();
 
         //从键盘接受todo
         Scanner scan = new Scanner(System.in);
@@ -18,16 +19,51 @@ public class Main {
         todo.title = title;
         todo.completed = false;
 
+        String task1 = "1 add tasks";
+        String task2 = "2 view tasks";
+        String task3 = "3 delete tasks";
+        String task4 = "4 exit";
 
-        //将从键盘接收到的todo添加进todo list
-        todoList.add(todo);
+        System.out.print(task1);
+        System.out.print( task2);
+        System.out.print( task3);
+        System.out.print( task4);
 
-        //打印出todo list里的每一个todo
-        for(Todo t : todoList) {
-            System.out.println("todo id:" + todo.id + ",todo title:" + todo.title + ",state:" + todo.completed);
-
+        switch(){
+            case "task1" :
+                addTask(todo);
+                break;
+            case "task2" :
+                viewTask();
+                break;
+            case "task3":
+                System.out.println("Which task you want to delete?");
+                deleteTask(scan.nextInt());
+            case "task4" :
+                scan.close();
         }
 
         scan.close();
+    }
+
+    //add task
+    public static void addTask(Todo todo) {
+        //将从键盘接收到的todo添加进todo list
+        todoList.add(todo);
+    }
+
+    //view tasks
+    public static String viewTask(){
+        //打印出todo list里的每一个todo
+        for(Todo t : todoList) {
+            System.out.println("todo id:" + todo.id + ",todo title:" + todo.title + ",state:" + todo.completed);
+        }
+        return "That's all tasks";
+    }
+
+    // delete tasks
+    public static void deleteTask(int id) {
+        todoList.remove(id);
+        viewTask();
     }
 }
